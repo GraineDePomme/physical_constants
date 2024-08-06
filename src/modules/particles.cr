2561293f0bc69e85d7a1d28335c2afc12f45e150
GraineDePomme <Guillaume.Sarisson@gmail.com>
2024-08-06 13:20:44 +0200

    adding reduced planck's constant

---
+++ src/modules/particles.cr
@@ -0,0 +1,15 @@
+module PhysicalConstants
+
+    module Electron
+
+        # The mass of one electron (in kg)
+        MASS = 9.109_383_713_9e-31
+
+        # The charge of an electron (in C)
+        CHARGE = - PhysicalConstants::ELEMENTARY_CHARGE
+
+    end
+
+
+
+end
--- src/physical_constants.cr
+++ src/physical_constants.cr
@@ -1,6 +1,7 @@
 require "./modules/astrophysics.cr"
 require "./modules/base.cr"
 require "./modules/periodic_table.cr"
+require "./modules/particles.cr"
 
 # TODO: Write documentation for `PhysicalConstants`
 module PhysicalConstants
